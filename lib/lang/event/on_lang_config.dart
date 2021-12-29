4deb261008553c218a43d163d967019493ce733b
root <yyinfo@gitlab>
2021-12-29 09:13:35 +0800

    删除多余的语言配置监听函数

--- lib/lang/event/on_lang_config.dart
+++ lib/lang/event/on_lang_config.dart
@@ -1,10 +1,6 @@
 import 'package:eyflutter_core/lang/beans/lang_config_entry.dart';
-import 'package:flutter/material.dart';
 
 mixin OnLangConfig {
   /// 语言配置列表
   List<LangConfigEntry> langEntries();
-
-  /// 支持本地化语言
-  List<Locale> supportedLocale();
 }
